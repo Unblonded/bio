660aa6e89dc8a4667df5d54be1562b7858ea9476
Unblonded <68570747+Unblonded@users.noreply.github.com>
2024-09-19 21:11:49 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-
+function toggleDescription(projectId) {
+    const projectDesc = document.getElementById(projectId);
+    if (projectDesc.style.display === "none") {
+        projectDesc.style.display = "block";
+    } else {
+        projectDesc.style.display = "none";
+    }
+}
